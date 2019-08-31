e39efcec00f4665ed6a677d7b25444098b0ab11c
Amanat <amanatjuwel@gmail.com>
2019-08-31 17:47:17 +0600

    user authentication added

---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It is a breeze. Simply tell Lumen the URIs it should respond to
+| and give it the Closure to call when that URI is requested.
+|
+*/
+
+$router->get('/', function () use ($router) {
+    return $router->app->version();
+});
+
+$router->group(['prefix'=>'api/v1'], function() use($router){
+	$router->get('/product', 'ProductController@index');
+	$router->post('/product', 'ProductController@create');
+	$router->get('/product/{id}', 'ProductController@show');
+	$router->put('/product/{id}', 'ProductController@update');
+	$router->delete('/product/{id}', 'ProductController@destroy');
+});
+
+$router->post('/login', 'LoginController@login');
+$router->post('/register', 'UserController@register');
+$router->get('/user', ['middleware' => 'auth', 'uses' =>  'UserController@get_user']);
